4d0e613770acf863603020cebf730b941e0f3fec
Armando Perez <armandlord98@gmail.com>
2023-10-04 18:46:49 -0600

    add: elementos del dom

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // Elementos del DOM
 //? obtener input
-
+const monedaInput = document.querySelector("input");
 //? obtener select
-
+const monedaSelect = document.querySelector("select");
 //? obtener boton
-
+const btn = document.querySelector("button");
 //? obtener span para el resultado
+const span = document.querySelector("span");
 
 // Variables
 //? url de api
