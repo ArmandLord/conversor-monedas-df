85f82472c6ac30189e721afbd1dc3fdf024d81eb
Armando Perez <armandlord98@gmail.com>
2023-10-04 18:42:41 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Elementos del DOM
+//? obtener input
+
+//? obtener select
+
+//? obtener boton
+
+//? obtener span para el resultado
+
+// Variables
+//? url de api
+
+//? variable para grafico
+
+// Eventos
+//? Evento para el boton
+
+// Funciones
+
+//? Busqueda
+
+//? render grafico
+
+//? crear data
+
+//? formatear fecha
